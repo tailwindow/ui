b713b54eca8c72a9949f6a57155939f2de453f79
Martien Dermawan Tanama <martiendt@gmail.com>
2020-10-03 10:30:32 +0700

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
