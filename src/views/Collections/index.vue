afc26d12f47fefe73d3ac3057b52f14eaa6594c7
Dimas Raka Septiawan <dimasrakas@gmail.com>
2020-10-15 23:54:05 +0700

    Add Contributor Collection Page

---
+++ src/views/Collections/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="bg-blue">
+    <div
+      class="container flex flex-col items-center h-screen px-5 pt-16 mx-auto md:flex-row"
+    >
+      <div
+        class="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0"
+      >
+        <h1
+          class="mb-4 font-bold leading-snug text-white font-primary text-display-5"
+        >
+          The Contributor Component Collection
+        </h1>
+        <p class="mb-8 text-lg font-light leading-relaxed text-white">
+          Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan
+          dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar
+          contoh teks sejak tahun 1500an.
+        </p>
+      </div>
+      <div class="w-full md:w-1/2">
+        <div class="flex items-center mb-5">
+          <div class="w-4/12 px-3 space-y-6">
+            <div>
+              <div
+                class="block p-5 my-3 mb-2 overflow-hidden transition-all duration-500 transform scale-100 rounded hover:scale-95"
+                style="
+                  background: url(https://images.unsplash.com/photo-1531537571171-a707bf2683da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)
+                    center;
+                  background-size: cover;
+                "
+              >
+                <div class="h-32"></div>
+              </div>
+            </div>
+
+            <div>
+              <div
+                class="block p-5 my-3 mb-2 overflow-hidden transition-all duration-500 transform scale-100 rounded hover:scale-95"
+                style="
+                  background: url(https://images.unsplash.com/photo-1531493731235-b5c309dca387?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80)
+                    center;
+                  background-size: cover;
+                "
+              >
+                <div class="h-48"></div>
+              </div>
+            </div>
+
+            <div>
+              <div
+                class="block p-5 my-3 mb-2 overflow-hidden transition-all duration-500 transform scale-100 rounded hover:scale-95"
+                style="
+                  background: url(https://images.unsplash.com/photo-1531538606174-0f90ff5dce83?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80)
+                    center;
+                  background-size: cover;
+                "
+              >
+                <div class="h-24"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-8/12 px-3 space-y-6">
+            <div>
+              <div
+                class="block my-3 mb-2 overflow-hidden transition-all duration-500 transform scale-100 bg-white rounded-lg shadow-2xl hover:scale-95"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1531538512164-e6c51ea63d20?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div>
+              <div
+                class="block my-3 mb-2 overflow-hidden transition-all duration-500 transform scale-100 rounded shadow-2xl hover:scale-95"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1531497865144-0464ef8fb9a9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
