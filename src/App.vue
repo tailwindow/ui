768e79f99e4b0b3bb4809e54acc15b4f39df0a14
Dimas Raka Septiawan <dimasrakas@gmail.com>
2020-10-15 23:55:29 +0700

    Scaffolding App.vue and Add custom CSS Transition

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="flex h-screen">
-    <!-- MENU SIDEBAR -->
-    <div class="w-64 bg-gray-500 p-4">
-      <div>LOGO</div>
-      <div class="py-2">
-        <input type="text" name="" id="" />
-      </div>
-      <app-menu></app-menu>
-    </div>
-    <!-- CONTENT -->
-    <div class="flex-1 p-4">
+  <div>
+    <MainNavigation />
+    <ComponentsNavigation v-if="$route.name.includes('Components')" />
+
+    <transition name="fade">
       <router-view />
-    </div>
+    </transition>
+
+    <Footer />
   </div>
 </template>
 
 <script>
-import AppMenu from "./layouts/AppMenu";
-
+import MainNavigation from "./layouts/MainNavigation";
+import ComponentsNavigation from "./layouts/ComponentsNavigation";
+import Footer from "./layouts/AppFooter";
 export default {
   components: {
-    AppMenu
+    MainNavigation,
+    ComponentsNavigation,
+    Footer
   }
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.55s, opacity 0.58s ease-in-out;
+}
+
+.fade-enter-from {
+  transform: scale(0.99);
+  opacity: 0;
+}
+.fade-leave-to {
+  transform: scale(1.03);
+  opacity: 0;
+}
+</style>
