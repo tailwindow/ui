5d26904ca808fb5c68599265e4b1fff6d5bf8b11
Dimas Raka Septiawan <dimasrakas@gmail.com>
2020-10-15 23:53:40 +0700

    Add Illustration Collection Page

---
+++ src/views/Illustrations/index.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="mt-24">
+    <div class="flex flex-col items-center px-16 md:flex-row">
+      <div class="w-1/2 mb-10 md:w-1/2 md:mb-0">
+        <img
+          class="object-cover object-center rounded"
+          alt="hero"
+          src="../../assets/illustrations/woman.svg"
+        />
+      </div>
+      <div
+        class="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left"
+      >
+        <h1
+          class="mb-4 text-4xl font-bold leading-snug text-gray-900 font-primary sm:text-4xl"
+        >
+          Awesome Illustration Collection
+        </h1>
+        <p class="mb-8 text-lg font-light leading-relaxed font-secondary">
+          Get in-depth information and UI resources for designing great apps
+          that integrate seamlessly with Apple platforms.
+        </p>
+        <div class="flex justify-center">
+          <button
+            class="inline-flex px-6 py-2 text-lg text-white transition duration-500 border-0 rounded bg-primary focus:outline-none hover:bg-primary-hover"
+          >
+            Read Documentation Now
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap px-2">
+      <div class="w-1/2 p-2">
+        <div class="flex flex-col items-center justify-center p-4 bg-light">
+          <div class="flex flex-col items-center justify-center my-8">
+            <p class="text-gray text-title-4">Eksternal Sources</p>
+            <h1 class="font-extrabold leading-none text-heading-1 font-primary">
+              Undraw
+            </h1>
+            <p class="mt-4 text-center">
+              Use images that scale without quality degradation, <br />making
+              them not just retina but future-ready.
+            </p>
+            <a
+              href="https://undraw.co/"
+              target="_blank"
+              class="mt-2 text-primary"
+              >Selengkapnya ></a
+            >
+          </div>
+          <img class="h-72" src="../../assets/illustrations/woman.svg" alt="" />
+        </div>
+      </div>
+      <div class="w-1/2 p-2">
+        <div class="flex flex-col items-center justify-center p-4 bg-light">
+          <div class="flex flex-col items-center justify-center my-8">
+            <p class="text-gray text-title-4">Eksternal Sources</p>
+            <h1 class="font-extrabold leading-none text-heading-1 font-primary">
+              Drawkit
+            </h1>
+            <p class="mt-4 text-center">
+              Lorem Ipsum is simply dummy text of <br />
+              the printing and typesetting industry.
+            </p>
+            <a
+              href="https://www.drawkit.io/"
+              target="_blank"
+              class="mt-2 text-primary"
+              >Selengkapnya ></a
+            >
+          </div>
+          <img class="h-72" src="../../assets/illustrations/man.svg" alt="" />
+        </div>
+      </div>
+      <div class="w-1/2 p-2">
+        <div class="flex flex-col items-center justify-center p-4 bg-light">
+          <div class="flex flex-col items-center justify-center my-8">
+            <p class="text-gray text-title-4">Eksternal Sources</p>
+            <h1 class="font-extrabold leading-none text-heading-1 font-primary">
+              Manypixels
+            </h1>
+            <p class="mt-4 text-center">
+              Use images that scale without quality degradation, <br />making
+              them not just retina but future-ready.
+            </p>
+            <a
+              href="https://www.manypixels.co/gallery/"
+              target="_blank"
+              class="mt-2 text-primary"
+              >Selengkapnya ></a
+            >
+          </div>
+          <img class="h-72" src="../../assets/illustrations/woman.svg" alt="" />
+        </div>
+      </div>
+      <div class="w-1/2 p-2">
+        <div class="flex flex-col items-center justify-center p-4 bg-light">
+          <div class="flex flex-col items-center justify-center my-8">
+            <p class="text-gray text-title-4">Eksternal Sources</p>
+            <h1 class="font-extrabold leading-none text-heading-1 font-primary">
+              Sapiens
+            </h1>
+            <p class="mt-4 text-center">
+              Use images that scale without quality degradation, <br />making
+              them not just retina but future-ready.
+            </p>
+            <a
+              href="https://sapiens.ui8.net/8m8c2s1"
+              target="_blank"
+              class="mt-2 text-primary"
+              >Selengkapnya ></a
+            >
+          </div>
+          <img class="h-72" src="../../assets/illustrations/woman.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Icons"
+};
+</script>
+
+<style></style>
