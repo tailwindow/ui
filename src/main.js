30722582c9e74317cf47c96547c460a2354304ea
Martien Dermawan Tanama <martiendt@gmail.com>
2020-11-15 15:35:44 +0700

    use external library

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ import router from "./router";
 import store from "./store";
 import "./assets/css/tailwind.css";
 
-import Highlight from "./lib/highlight.js";
+import Highlight from "@point-hub/vue-highlight";
 import "highlight.js/styles/agate.css";
+
+// Import Languages
+import javascript from "highlight.js/lib/languages/javascript";
+import html from "highlight.js/lib/languages/xml";
+
+// Register Language
+Highlight.registerLanguage("html", html);
+Highlight.registerLanguage("javascript", javascript);
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(Highlight)
+  .use(Highlight.plugin)
   .mount("#app");
